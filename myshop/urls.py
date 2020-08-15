167ba1c307436812a54a0414fa7ddf9bdc58925f
SDeVPro <sardor5676626@gmail.com>
2020-08-15 22:16:44 +0500

    Add UserProfile and create shopcart function on order app views

--- myshop/urls.py
+++ myshop/urls.py
@@ -19,10 +19,13 @@ from django.contrib import admin
 from django.urls import path, include
 
 from home import views
+from order import views as OrderViews
 urlpatterns = [
     path('product/', include('product.urls')),
     path('', include('home.urls')),
     path('order/', include('order.urls')),
+    path('user/', include('user.urls')),
+    path('shopcart/', OrderViews.shopcart, name='shopcart'),
 
     path('home/', include('home.urls')),
     path('search/', views.search, name='search'),
--- order/views.py
+++ order/views.py
@@ -1,9 +1,11 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 from order.models import ShopCart, ShopCartForm
+from product.models import Category
 
 
 def index(request):
@@ -46,3 +48,24 @@ def addtoshopcart(request, id):
             data.save()
         messages.success(request, "Mahsulot savatchaga qo'shildi")
         return HttpResponseRedirect(url)
+
+
+def shopcart(request):
+    category = Category.objects.all()
+    current_user = request.user
+    shopcart = ShopCart.objects.filter(user_id=current_user.id)
+    total = 0
+    for rs in shopcart:
+        total += rs.product.price * rs.quantity
+    context = {
+        'category': category,
+        'shopcart': shopcart,
+        'total':total,
+    }
+    return render(request, 'shopcart.html', context)
+
+@login_required(login_url='/login')
+def deletefromcart(request, id):
+    ShopCart.objects.filter(id=id).delete()
+    messages.success(request, "Sizning mahsulotingiz savatdan o'chirildi!")
+    return HttpResponseRedirect('/shopcart')
